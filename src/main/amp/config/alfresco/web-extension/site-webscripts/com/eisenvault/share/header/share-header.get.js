e1635545d33938e166db17cd459115e54482fc46
Vipul Swarup <vipul.swarup@eisenvault.com>
2015-08-03 11:46:32 +0530

    Initial commit

---
+++ src/main/amp/config/alfresco/web-extension/site-webscripts/com/eisenvault/share/header/share-header.get.js
@@ -0,0 +1,13 @@
+var sitesMenu = widgetUtils.findObject(model.jsonModel, "id", "HEADER_SITES_MENU");
+sitesMenu.config.label="Department";
+sitesMenu.config.mySitesLabel="My Departments";
+sitesMenu.config.siteFinderLabel="Department Finder";
+sitesMenu.config.createSiteLabel="Create Department";
+sitesMenu.config.recentGroupLabel="Recent Departments";
+
+
+
+if(page.titleId == "page.userSites.title"){
+	var userSitesTitle = widgetUtils.findObject(model.jsonModel, "id", "HEADER_TITLE");
+	userSitesTitle.config.label = "User Departments List";
+}
---
+++ target/test-classes/alfresco/site-webscripts/org/alfresco/share/pages/faceted-search/faceted-search.get.html.ftl
@@ -0,0 +1,2 @@
+<@processJsonModel group="share"/>
+<@link rel="stylesheet" type="text/css" href="${page.url.context}/res/resources/hide.css" />
