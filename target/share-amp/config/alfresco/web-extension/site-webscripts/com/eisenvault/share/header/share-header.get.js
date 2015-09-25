c0ddf7c0512c5f7243df1ba739671a7f06a01cb5
Vipul Swarup <vipul.swarup@eisenvault.com>
2015-09-25 12:50:32 +0530

    Undid create department changes

--- target/share-amp/config/alfresco/web-extension/site-webscripts/com/eisenvault/share/header/share-header.get.js
+++ target/share-amp/config/alfresco/web-extension/site-webscripts/com/eisenvault/share/header/share-header.get.js
@@ -5,8 +5,6 @@ sitesMenu.config.siteFinderLabel="Department Finder";
 sitesMenu.config.createSiteLabel="Create Department";
 sitesMenu.config.recentGroupLabel="Recent Departments";
 
-
-
 if(page.titleId == "page.userSites.title"){
 	var userSitesTitle = widgetUtils.findObject(model.jsonModel, "id", "HEADER_TITLE");
 	userSitesTitle.config.label = "User Departments List";
